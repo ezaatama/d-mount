7047ee84e156ac58ca46ab118051cb6f072db3c9
reza putra <ezatama33@gmail.com>
2021-08-13 10:48:44 +0700

    styling theme

---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Color Section
+Color primaryColor = Color(0x055052);
+Color backgroundColor = Color(0x58bbbb);
+Color darkColor = Color(0x2d2928);
+Color whiteColor = Colors.white;
+Color blackColor = Colors.black;
+
+/// FONT WEIGHT
+FontWeight light = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+//Styling Section
+TextStyle textBlueButton = GoogleFonts.nunito(
+  color: whiteColor,
+);
+
+TextStyle textWhiteButton = GoogleFonts.nunito(
+  color: primaryColor,
+);
+
+TextStyle blackTitle = GoogleFonts.nunito(
+  color: blackColor,
+);
